00ca6d60e8f2dda6be9898387683ee92ce4992d9
Pliskinzc <pliskinzc@gmail.com>
2023-07-30 20:34:41 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT | '3000';
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(
+  'mongodb+srv://aziz:aziza@cluster0.8kfotz7.mongodb.net/?retryWrites=true&w=majority'
+);
+client
+  .connect()
+  .then(() => {
+    console.log('connected !');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express.static('../client-build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client-build/index.html'))
+})
+
+app.listen(port);
